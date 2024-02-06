25aa4e309108dcba16df231d6032eb7e8654b077
Christopher Lamm <christopher.lamm@companycam.com>
2024-02-06 10:30:54 -0500

    commented out state, memo, and effect

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,39 +1,37 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Text, StyleSheet} from 'react-native';
 
 // Passes in a property called text and deconstructing it.
 const Title = ({text}) => {
   // Sets up a variable in state with a default value.
-  const [stateText, setStateText] = useState('Default State');
+  // const [stateText, setStateText] = useState('Default State');
 
   // Called anytime something changes.
-  useEffect(
-    () => {
-      // What you want to do when something changes.
-    },
-    [
-      // Define items you want to listen to.
-      // Empty array will only do it once.
-    ],
-  );
+  // useEffect(
+  //   () => {
+  //     // What you want to do when something changes.
+  //   },
+  //   [
+  //     // Define items you want to listen to.
+  //     // Empty array will only do it once.
+  //   ],
+  // );
 
   // Updates the stateText when the function is called.
-  const onTextPress = () => {
-    setStateText('Updated State');
-  };
+  // const onTextPress = () => {
+  //   setStateText('Updated State');
+  // };
 
   return (
     // onPress calls the function onTextPress when the component is clicked.
-    <Text onPress={onTextPress} style={styles.title}>
-      {stateText}
-    </Text>
+    <Text /* onPress={onTextPress} */ style={styles.title}>{text}</Text>
   );
 };
 
 // Sets a default value for the text property.
-Title.defaultProps = {
-  text: 'Default text',
-};
+// Title.defaultProps = {
+//   text: 'Default text',
+// };
 
 const styles = StyleSheet.create({
   title: {
