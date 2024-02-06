89e902f626d82899092533c956e0446b70856aad
Christopher Lamm <christopher.lamm@companycam.com>
2024-02-06 12:16:30 -0500

    added main and sub title to home page

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,15 +1,31 @@
+/* eslint-disable react-native/no-inline-styles */
+
 import React from 'react';
-import {View, SafeAreaView} from 'react-native';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
 import Title from '../../components/title';
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Title text="My First Component" />
+      <View style={styles.container}>
+        <Title text="Where do" style={{fontWeight: 'normal'}} />
+        <Title text="you want to go?" />
+        <Title text="Explore Attractions" style={styles.subtitle} />
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 32,
+  },
+  subtitle: {
+    fontSize: 20,
+    color: '#000000',
+    marginTop: 40,
+    marginBottom: 34,
+  },
+});
 
 export default React.memo(Home);
