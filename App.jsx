705310a726489551f9f7fb0c9cfd22b1faf5a748
Christopher Lamm <christopher.lamm@companycam.com>
2024-02-20 08:58:49 -0500

    setup React Navigation

--- App.jsx
+++ App.jsx
@@ -1,8 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import Home from './src/screens/home';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
-  return <Home />;
+  <NavigationContainer>
+    return <Home />;
+  </NavigationContainer>;
 };
 
 export default App;
